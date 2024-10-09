e9cbc1f1b40061954f7eef26ebb3b2744f39da6f
rovifran <frovira@fi.uba.ar>
2024-10-09 16:08:26 -0300

    Changed reducer query 1 to use middleware results

--- reducer/reducer-queries/reducer-query-1.go
+++ reducer/reducer-queries/reducer-query-1.go
@@ -10,7 +10,6 @@ var log = logging.MustGetLogger("log")
 
 type ReducerQuery1 struct {
 	middleware *middleware.Middleware
-	// pendingAnswers  int no se si es necesario este campo para la query 1
 	Windows int64
 	Mac     int64
 	Linux   int64
@@ -29,37 +28,49 @@ func (r *ReducerQuery1) Close() {
 func (r *ReducerQuery1) Run() {
 	log.Infof("Reducer Query 1 running")
 
-	resultsQueue, err := r.middleware.ListenGames("") // esto despues va a ser listenResults
+	resultsQueue, err := r.middleware.ListenResults("")
 	if err != nil {
 		log.Fatalf("action: listen reviews| result: error | message: %s", err)
 		return
 	}
 
-	resultsQueue.Consume(func(msg *middleware.GameBatch, ack func()) error {
-		// r.processResult(&game)
-		log.Infof("Game: %v", msg.Game)
+	resultsQueue.Consume(func(msg *middleware.Result, ack func()) error {
+		r.processResult(msg)
 
 		ack()
 
+		r.SendResult(msg.IsFinalMessage)
 		return nil
 	})
 }
 
-func (r *ReducerQuery1) processResult(game *middleware.Query1Result) { // despues este interface va a ser el result
-	r.Windows += game.Windows
-	r.Mac += game.Mac
-	r.Linux += game.Linux
+func (r *ReducerQuery1) processResult(result *middleware.Result) {
+	switch result.Payload.(type) {
+	case middleware.Query1Result:
+		query1Result := result.Payload.(middleware.Query1Result)
+		r.Windows += query1Result.Windows
+		r.Mac += query1Result.Mac
+		r.Linux += query1Result.Linux
+	}
+
 }
 
-func (r *ReducerQuery1) SendResult() {
-	// result := &middleware.Query1Result{
-	// 	Windows: r.Windows,
-	// 	Mac:     r.Mac,
-	// 	Linux:   r.Linux,
-	// }
+func (r *ReducerQuery1) SendResult(isFinalMessage bool) {
+	query1Result := &middleware.Query1Result{
+		Windows: r.Windows,
+		Mac:     r.Mac,
+		Linux:   r.Linux,
+	}
 
-	// err := r.middleware.SendQuery1Result(result)
-	// if err != nil {
-	// 	log.Errorf("Failed to send result: %v", err)
-	// }
+	result := &middleware.Result{
+		QueryId:             1,
+		Payload:             query1Result,
+		IsFragmentedMessage: false,
+		IsFinalMessage:      isFinalMessage,
+	}
+
+	err := r.middleware.SendResult("1", result)
+	if err != nil {
+		log.Errorf("Failed to send result: %v", err)
+	}
 }
