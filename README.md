1974ee5b19a0ef558749a53ddf0c7187a08f8453
rovifran <frovira@fi.uba.ar>
2024-10-28 18:57:45 -0300

    Changed validate script to receive files via arguments

--- README.md
+++ README.md
@@ -7,9 +7,7 @@ Importantes:
 - [ ] Mapper: archivo de reviews (aka lo de franco)
 - [ ] Queries: mover 1 y 2 a disco
 - [ ] Reducer: mover 1, 2 y 3 a disco
-- [ ] Cliente: configurar donde escribe sus resultados por env.
 - [ ] Queries: al recibir EOF, cerrar el directorio del cliente
 - [ ] Reducer: al recibir EOF, cerrar el directorio del cliente
-- [ ] Validador reciba por parametro el archivo de verificacion y el result del cliente
 - [ ] Actualizar informe
 - [ ] Actualizar logs en gral
--- results/validate.py
+++ results/validate.py
@@ -1,118 +1,131 @@
 import json
+import argparse
 
-results_file = "./results_m.json"
+def main():
+    parser = argparse.ArgumentParser(description='Validate results.')
+    parser.add_argument('-b', '--base', required=True, help='Path to the base results file')
+    parser.add_argument('-f', '--file', required=True, help='Path to the file to compare')
+    args = parser.parse_args()
 
-with open(results_file, 'r') as file:
-    data = json.load(file)
+    results_file = args.base
+    compare_file = args.file
+    # results_file = "./results_m.json"
+    # compare_file = "./results.txt"
 
-with open('./results.txt', 'r') as file:
-    tp_results = file.readlines()
+    with open(results_file, 'r') as file:
+        data = json.load(file)
 
-print("Checking q1...")
+    with open(compare_file, 'r') as file:
+        tp_results = file.readlines()
 
-tp_q1 = ""
-for line in tp_results:
-    if line.startswith("[QUERY 1 - FINAL]:"):
-        tp_q1 = line.split("[QUERY 1 - FINAL]: ")[1].strip()
-        break
+    print("Checking q1...")
 
-if tp_q1 == "Windows: " +str(data['q1']['windows'])+", Mac: " +str(data['q1']['mac'])+", Linux: " +str(data['q1']['linux']):
-    print("q1 is correct")
-else:
-    print("Expected: ", data['q1'])
-    print("Found: ", tp_q1)
-    print("q1 is incorrect")
-
-
-print("\nChecking q2...")
-
-ok = True
-for i, top_game in enumerate(data['q2']):
-    found = False
+    tp_q1 = ""
     for line in tp_results:
-        if line.startswith("[QUERY 2]: Top Game "+str(i+1)+": "+top_game['Name'] + " ("+str(top_game['Average playtime forever'])+")"):
-            found = True
+        if line.startswith("[QUERY 1 - FINAL]:"):
+            tp_q1 = line.split("[QUERY 1 - FINAL]: ")[1].strip()
             break
-    if not found:
-        print("Expected: ", top_game['Name'])
+
+    if tp_q1 == "Windows: " +str(data['q1']['windows'])+", Mac: " +str(data['q1']['mac'])+", Linux: " +str(data['q1']['linux']):
+        print("q1 is correct")
+    else:
+        print("Expected: ", data['q1'])
+        print("Found: ", tp_q1)
+        print("q1 is incorrect")
+
+
+    print("\nChecking q2...")
+
+    ok = True
+    for i, top_game in enumerate(data['q2']):
+        found = False
+        for line in tp_results:
+            if line.startswith("[QUERY 2]: Top Game "+str(i+1)+": "+top_game['Name'] + " ("+str(top_game['Average playtime forever'])+")"):
+                found = True
+                break
+        if not found:
+            print("Expected: ", top_game['Name'])
+            ok = False
+
+    if ok:
+        print("q2 is correct")
+    else:
+        print("q2 is incorrect")
+
+    print("\nChecking q3...")
+
+    ok = True
+    for i, top_game in enumerate(data['q3']):
+        found = False
+        for line in tp_results:
+            if line.startswith("[QUERY 3]: Top Game "+str(i+1)+": "+top_game['Name'] + " ("+str(top_game['positive_score'])+")"):
+                found = True
+                break
+        if not found:
+            print("Expected: ", top_game['Name'])
+            ok = False
+
+    if ok:
+        print("q3 is correct")
+    else:
+        print("q3 is incorrect")
+
+    print("\nChecking q4...")
+
+    ok = True
+    for i, top_game in enumerate(data['q4']):
+        found = False
+        for line in tp_results:
+            if line.startswith("[QUERY 4 - PARCIAL]: "+top_game['Name']):
+                found = True
+                break
+        if not found:
+            print("Expected: ", top_game['Name'])
+            ok = False
+
+    total_games = 0
+    for line in tp_results:
+        if line.startswith("[QUERY 4 - PARCIAL]:"):
+            total_games += 1
+
+    if total_games != len(data['q4']):
+        print("Expected total games: ", len(data['q4']))
+        print("Found total games: ", total_games)
         ok = False
 
-if ok:
-    print("q2 is correct")
-else:
-    print("q2 is incorrect")
+    if ok:
+        print("q4 is correct")
+    else:
+        print("q4 is incorrect")
 
-print("\nChecking q3...")
 
-ok = True
-for i, top_game in enumerate(data['q3']):
-    found = False
+    print("\nChecking q5...")
+
+    ok = True
+    for i, top_game in enumerate(data['q5']):
+        found = False
+        for line in tp_results:
+            if line.startswith("[QUERY 5 - PARCIAL]: "+top_game['Name'] + " ("+str(top_game['count'])+")"):
+                found = True
+                break
+        if not found:
+            print(f"Expected: {top_game['Name']} with {top_game["count"]} negative reviews")
+            ok = False
+
+    total_games = 0
     for line in tp_results:
-        if line.startswith("[QUERY 3]: Top Game "+str(i+1)+": "+top_game['Name'] + " ("+str(top_game['positive_score'])+")"):
-            found = True
-            break
-    if not found:
-        print("Expected: ", top_game['Name'])
+        if line.startswith("[QUERY 5 - PARCIAL]:"):
+            total_games += 1
+
+    if total_games != len(data['q5']):
+        print("Expected total games: ", len(data['q5']))
+        print("Found total games: ", total_games)
         ok = False
 
-if ok:
-    print("q3 is correct")
-else:
-    print("q3 is incorrect")
+    if ok:
+        print("q5 is correct")
+    else:
+        print("q5 is incorrect")
 
-print("\nChecking q4...")
-
-ok = True
-for i, top_game in enumerate(data['q4']):
-    found = False
-    for line in tp_results:
-        if line.startswith("[QUERY 4 - PARCIAL]: "+top_game['Name']):
-            found = True
-            break
-    if not found:
-        print("Expected: ", top_game['Name'])
-        ok = False
-
-total_games = 0
-for line in tp_results:
-    if line.startswith("[QUERY 4 - PARCIAL]:"):
-        total_games += 1
-
-if total_games != len(data['q4']):
-    print("Expected total games: ", len(data['q4']))
-    print("Found total games: ", total_games)
-    ok = False
-
-if ok:
-    print("q4 is correct")
-else:
-    print("q4 is incorrect")
-
-
-print("\nChecking q5...")
-
-ok = True
-for i, top_game in enumerate(data['q5']):
-    found = False
-    for line in tp_results:
-        if line.startswith("[QUERY 5 - PARCIAL]: "+top_game['Name'] + " ("+str(top_game['count'])+")"):
-            found = True
-            break
-    if not found:
-        print(f"Expected: {top_game['Name']} with {top_game["count"]} negative reviews")
-        ok = False
-
-total_games = 0
-for line in tp_results:
-    if line.startswith("[QUERY 5 - PARCIAL]:"):
-        total_games += 1
-
-if total_games != len(data['q5']):
-    print("Expected total games: ", len(data['q5']))
-    print("Found total games: ", total_games)
-    ok = False
-
-if ok:
-    print("q5 is correct")
-else:
-    print("q5 is incorrect")
+if __name__ == "__main__":
+    main()
